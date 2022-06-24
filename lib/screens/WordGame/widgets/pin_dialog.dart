62b3a29d39aee04759a70766470ec734d8647f12
Omer Mert Hayir <mermert251@gmail.com>
2022-06-24 04:39:49 +0300

    pin pop up and recovery pop up

--- lib/screens/WordGame/widgets/pin_dialog.dart
+++ lib/screens/WordGame/widgets/pin_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hidden_hiding_app/models/user_data.dart';
+import 'package:hidden_hiding_app/screens/WordGame/widgets/secret_words_dialog.dart';
+import 'package:hidden_hiding_app/services/storage_service.dart';
 
 class PinDialog extends StatefulWidget {
   final bool isPasswordSet;
@@ -13,39 +16,56 @@ class PinDialog extends StatefulWidget {
 }
 
 class _PinDialogState extends State<PinDialog> {
+  var encryptedStorage = StorageService();
   var digit1 = TextEditingController();
   var digit2 = TextEditingController();
   var digit3 = TextEditingController();
   var digit4 = TextEditingController();
+  int wrongPinCount = 0;
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      backgroundColor: Colors.amber[300],
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Text(widget.isPasswordSet
-                ? "Please enter four digits to create your secret pin"
-                : "Please enter your secret pin"),
+            child: Text(
+              widget.isPasswordSet
+                  ? "Please enter four digits to create your secret pin"
+                  : "Please enter your secret pin",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.abel(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              pinInputFields(digit1),
-              pinInputFields(digit2),
-              pinInputFields(digit3),
-              pinInputFields(digit4),
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                pinInputFields(digit1, "digit1"),
+                pinInputFields(digit2, "digit2"),
+                pinInputFields(digit3, "digit3"),
+                pinInputFields(digit4, "digit4"),
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              interactionButtons("Clear"),
-              interactionButtons("Submit"),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                interactionButtons("Clear"),
+                interactionButtons("Submit"),
+              ],
+            ),
           ),
         ],
       ),
@@ -62,11 +82,49 @@ class _PinDialogState extends State<PinDialog> {
           ),
           primary: Colors.orange[500],
           onPrimary: Colors.black),
-      onPressed: () {
+      onPressed: () async {
         switch (buttonName) {
           case "Clear":
+            digit1.clear();
+            digit2.clear();
+            digit3.clear();
+            digit4.clear();
             break;
           case "Submit":
+            if (widget.isPasswordSet) {
+              await encryptedStorage.writeSecureData(UserData(
+                  key: "secretPin",
+                  value:
+                      "${digit1.text},${digit2.text},${digit3.text},${digit4.text}"));
+              showDialog(
+                  context: context,
+                  builder: (context) => SecretWordsDialog(
+                        isPasswordSet: widget.isPasswordSet,
+                        isRecoveryMode: false,
+                      ));
+            } else {
+              await encryptedStorage
+                  .readSecureData("secretPin")
+                  .then((secretPin) {
+                if (secretPin!.split(",")[0] == digit1.text &&
+                    secretPin.split(",")[1] == digit2.text &&
+                    secretPin.split(",")[2] == digit3.text &&
+                    secretPin.split(",")[3] == digit4.text) {
+                  Navigator.of(context).pushNamed("/VaultMainScreen");
+                } else {
+                  wrongPinCount++;
+                  if (wrongPinCount == 3) {
+                    wrongPinCount = 0;
+                    showDialog(
+                        context: context,
+                        builder: (context) => const SecretWordsDialog(
+                              isPasswordSet: false,
+                              isRecoveryMode: true,
+                            ));
+                  }
+                }
+              });
+            }
             break;
           default:
             return;
@@ -76,11 +134,17 @@ class _PinDialogState extends State<PinDialog> {
     );
   }
 
-  Widget pinInputFields(TextEditingController controller) {
+  Widget pinInputFields(TextEditingController controller, String fieldName) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: TextField(
         controller: controller,
+        obscureText: true,
+        autofocus: true,
+        cursorColor: Colors.black,
+        cursorWidth: 3,
+        textInputAction:
+            fieldName == "digit4" ? TextInputAction.done : TextInputAction.next,
         keyboardType: TextInputType.number,
         textAlign: TextAlign.center,
         style: GoogleFonts.abel(
@@ -88,8 +152,6 @@ class _PinDialogState extends State<PinDialog> {
           fontWeight: FontWeight.bold,
           color: Colors.black,
         ),
-        cursorColor: Colors.black,
-        cursorWidth: 3,
         decoration: InputDecoration(
           constraints: const BoxConstraints(
             maxWidth: 50,
--- lib/screens/WordGame/widgets/secret_words_dialog.dart
+++ lib/screens/WordGame/widgets/secret_words_dialog.dart
@@ -1,11 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hidden_hiding_app/models/user_data.dart';
+import 'package:hidden_hiding_app/screens/WordGame/src/recovery_words.dart';
+import 'package:hidden_hiding_app/services/storage_service.dart';
 
 class SecretWordsDialog extends StatefulWidget {
   final bool isPasswordSet;
+  final bool isRecoveryMode;
   const SecretWordsDialog({
     Key? key,
     required this.isPasswordSet,
+    required this.isRecoveryMode,
   }) : super(key: key);
 
   @override
@@ -13,6 +20,32 @@ class SecretWordsDialog extends StatefulWidget {
 }
 
 class _PinDialogState extends State<SecretWordsDialog> {
+  var encryptedStorage = StorageService();
+  List<String> selectedRecoveryWords = [];
+  var rng = Random();
+  bool isNotValid = false;
+  List<TextEditingController> recoveryFields = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  @override
+  void initState() {
+    for (int itemIndex = 0; itemIndex < 8; itemIndex++) {
+      List<String> list = nouns;
+      int rngIndex = rng.nextInt(list.length);
+      selectedRecoveryWords.add(list[rngIndex]);
+      list.removeAt(rngIndex);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -24,15 +57,78 @@ class _PinDialogState extends State<SecretWordsDialog> {
             padding: const EdgeInsets.all(16.0),
             child: Text(
               widget.isPasswordSet
-                  ? "Please backup all of the given words in order to be able to recover your vault in case you forgot your combination or secret pin"
+                  ? "Please backup all of the given words in order to be able to recover your vault in case you forgot your combination or secret pin, if you lost it you cannot recover any data so be aware of that"
                   : "Please enter your recovery keys respectively",
               textAlign: TextAlign.center,
             ),
           ),
-          // TODO generate secret keys or get input from user to recover vault
+          if (widget.isPasswordSet)
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.amber[700]!,
+                  width: 2,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Wrap(
+                  children: List.generate(
+                      selectedRecoveryWords.length,
+                      (index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(selectedRecoveryWords[index]),
+                          )),
+                ),
+              ),
+            ),
+          if (widget.isRecoveryMode)
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.amber[700]!,
+                  width: 2,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Wrap(
+                  children: List.generate(
+                      selectedRecoveryWords.length,
+                      (index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: recoveryInputFields(
+                                recoveryFields[index], "key$index"),
+                          )),
+                ),
+              ),
+            ),
+          if (isNotValid)
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "keys are not correct! Please try again",
+                textAlign: TextAlign.center,
+              ),
+            ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              if (widget.isRecoveryMode)
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: GoogleFonts.abel(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      primary: Colors.orange[500],
+                      onPrimary: Colors.black),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Try Again"),
+                ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     textStyle: GoogleFonts.abel(
@@ -42,12 +138,83 @@ class _PinDialogState extends State<SecretWordsDialog> {
                     ),
                     primary: Colors.orange[500],
                     onPrimary: Colors.black),
-                onPressed: () {},
-                child: const Text("Understood"),
+                onPressed: () async {
+                  if (widget.isPasswordSet) {
+                    await encryptedStorage.writeSecureData(UserData(
+                        key: "recoveryKeys",
+                        value:
+                            "${selectedRecoveryWords[0]},${selectedRecoveryWords[1]},${selectedRecoveryWords[2]},${selectedRecoveryWords[3]},${selectedRecoveryWords[4]},${selectedRecoveryWords[5]},${selectedRecoveryWords[6]},${selectedRecoveryWords[7]}"));
+                    Navigator.of(context).pushNamed("/VaultMainScreen");
+                  } else {
+                    await encryptedStorage
+                        .readSecureData("recoveryKeys")
+                        .then((recoveryKeys) {
+                      if (recoveryKeys!.split(",")[0] ==
+                              recoveryFields[0].text &&
+                          recoveryKeys.split(",")[1] ==
+                              recoveryFields[1].text &&
+                          recoveryKeys.split(",")[2] ==
+                              recoveryFields[2].text &&
+                          recoveryKeys.split(",")[3] ==
+                              recoveryFields[3].text &&
+                          recoveryKeys.split(",")[4] ==
+                              recoveryFields[4].text &&
+                          recoveryKeys.split(",")[5] ==
+                              recoveryFields[5].text &&
+                          recoveryKeys.split(",")[6] ==
+                              recoveryFields[6].text &&
+                          recoveryKeys.split(",")[7] ==
+                              recoveryFields[7].text) {
+                        Navigator.of(context).pushNamed("/VaultMainScreen");
+                      } else {
+                        setState(() {
+                          isNotValid = true;
+                        });
+                      }
+                    });
+                  }
+                },
+                child: Text(widget.isPasswordSet ? "Understood" : "Recover"),
               ),
             ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget recoveryInputFields(
+      TextEditingController controller, String fieldName) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: TextField(
+        controller: controller,
+        obscureText: true,
+        autofocus: true,
+        cursorColor: Colors.black,
+        cursorWidth: 3,
+        textInputAction:
+            fieldName == "digit4" ? TextInputAction.done : TextInputAction.next,
+        keyboardType: TextInputType.name,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.abel(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+        decoration: InputDecoration(
+          constraints: const BoxConstraints(
+            maxWidth: 90,
+          ),
+          filled: true,
+          fillColor: const Color(0xFFF2F2F2),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2, color: Colors.amber[700]!),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2, color: Colors.amber[700]!),
+          ),
+        ),
       ),
     );
   }
